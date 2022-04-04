673668d546c1636c0c729bf0beacbe295edf6aa7
Anıl TOPUZ <aniltopuz@gmail.com>
2022-04-04 19:50:11 +0300

    Update Lesson

---
+++ src/oop/kalitim/main.java
@@ -0,0 +1,7 @@
+package oop.kalitim;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/oop/poliformizm/main.java
@@ -0,0 +1,7 @@
+package oop.poliformizm;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
