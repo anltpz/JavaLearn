6634cd4799d38fd4cb24c60e51d7a35df722867c
Anıl TOPUZ <aniltopuz@gmail.com>
2022-04-04 19:26:59 +0300

    Primitive tip ve karmaşık tip karşılastırmasi

---
+++ src/tam_sayilar/main.java
@@ -0,0 +1,70 @@
+package tam_sayilar;
+
+public class main {
+    public static void main(String[] args) {
+
+        // Değiskenler degerlere atanır
+        //Bu değerler üzerinden işlemler yapılır
+        // veriler işlemcilerde işlenir ve yeni degerler üretilir
+
+        // Tip Nedir ?
+        //
+        // Bir değişkenin alabilecegi degerler kümesine tip denir
+        //Tipler degerler kümesinin tabiatına uygun kurallara sahiptirler
+        //
+
+        // tipler 2 ye ayrılır
+         //  1-İlkel tipler
+         // 2- Karmaşık tipler
+
+        // İlkel tipler
+        // tek degerden olusur
+        //metotları yoktur
+        // int  a=5 metodu yoktur asagıdaki car örneginde Car sınıfının sorumlulukları vardır
+        // atomictir parcalayamayız
+
+            // ilkel tip sıralması
+        // byte, short , int long , float , double ,char , boolean
+        // sadece boolean mantıksal bir tip geri kalanlar sayısaldır
+        // char dısında diger bütün 6 sayısal tip de işaretlidir
+        // pozitif ve negatif deger alabilir
+        //char karakter tipidir ve her karakterin sıfır yada pozitif tam sayı degeri vardır
+
+        // 6 sayısal tipin 4'ü tam sayı 2 si gerçek sayıdır
+        //Tam sayılar
+        // 1-byte ,
+        // 2-short ,
+        // 3-int
+        // 4-long
+
+        //gercek sayılar
+        // 1-float
+        // 2-double
+
+        //Complex tip
+        // parcaları vardır
+        // ilkel ve bileşik tipten olusur
+        //class enum ve interface anahtar kelimeleri ile olusturulabilir
+
+
+        System.out.println("Hello World!");
+
+    }
+
+}
+class Car
+{
+    public String make;  // sınıf tipidir  --> String, public final class String tipinin nesnesidir
+    public String model; //sınıf tipidir
+    public String year; // sınıf tipidir
+    public int speed;  // ilkel tip
+    public int distance ; // ilkel tip
+
+    //Metotları olabilir
+
+    public void stop()
+    {
+        speed=0;
+    }
+
+}
